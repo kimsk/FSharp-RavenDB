200ed3a96a05166c9cfc6f86d62e32ad9d173748
Karlkim Suwamongkol <karlkim@outlook.com>
2014-03-06 22:22:20 -0600

    more refactoring
    - toTweet and toFollower instead of getTweet and getFollwer
    - saveNewTweets function to replace addNewTweets

--- src/FSharpConsole/Program.fs
+++ src/FSharpConsole/Program.fs
@@ -41,7 +41,7 @@ let getMentions (s : Status) =
     |> Seq.map (fun x -> x.ScreenName.ToLower())
     |> Array.ofSeq
 
-let getTweet (s : Status) = 
+let toTweet (s : Status) = 
     { StatusId = s.StatusID      
       Text = s.Text
       RetweetCount = s.RetweetCount
@@ -123,16 +123,18 @@ let getAllTweetsFromRavenDB (session:IDocumentSession) q =
     getTweets [] 1
 
 
-let addNewFollowers (session:IDocumentSession) =
+let toFollower (f:User) =
+    {
+        Id = f.ScreenName
+        ScreenName = f.ScreenNameResponse
+        Name = f.Name
+        Location = f.Location
+        Description = f.Description
+    }
+
+let saveNewFollowers (session:IDocumentSession) =
     // followers
-    let followers = getAllFollowers "kimsk" |> List.map (fun f -> 
-                                            {
-                                                Id = f.ScreenName
-                                                ScreenName = f.ScreenNameResponse
-                                                Name = f.Name
-                                                Location = f.Location
-                                                Description = f.Description
-                                            })
+    let followers = getAllFollowers "kimsk" |> List.map toFollower
 
     // store followers    
     let isMyFollower screenName =
@@ -152,40 +154,23 @@ let addNewFollowers (session:IDocumentSession) =
     printfn "%d new followers" newFollowers.Length
     session.SaveChanges()
 
- 
-let addNewTweets (session:IDocumentSession) =
-    // add new tweets since last tweet
-    let lastTweet = session.Query<Tweet>().OrderByDescending(fun t -> t.CreatedAt).First()
-    let lastStatusId = lastTweet.StatusId
-    let q = 
-        ctx.Status.Where(fun s -> s.Type = StatusType.User && s.ScreenName = "kimsk" && s.Count = 200 && s.SinceID = lastStatusId)
-        |> Seq.map getTweet
-        |> Seq.sortBy (fun t -> t.CreatedAt)
-        
-    printfn "%A" (q |> Seq.head)
 
-    for tweet in q do
+let saveNewTweets (session:IDocumentSession) tweetType allTweets = 
+    let isNewTweet statusId =
+        not <| session.Query<Tweet>().Any(fun t -> t.StatusId = statusId)
+
+    let addIfNewTweet acc tweet =
+        if isNewTweet tweet.StatusId then
+            tweet::acc
+        else
+            acc
+
+    let newTweets = allTweets |> List.fold addIfNewTweet []
+
+    for tweet in newTweets do
         session.Store(tweet)
 
-    session.SaveChanges()
-
-let addNewFSharpTweets (session:IDocumentSession) =
-    let allFSharpTweets = getAllTweetsFromRavenDB session "fsharp"
-    let fsharpSinceId = allFSharpTweets.Max(fun t -> t.StatusId)
-    printfn "%d %A" (allFSharpTweets.Count()) (allFSharpTweets.Where(fun t -> t.StatusId = fsharpSinceId))
-
-    printfn "%A" ((getSearchResultWithSinceId "#fsharp" 1 (fsharpSinceId - 1000UL)).Statuses.First().Text)
-    
-    let fsharpTweets = getStatuses "#fsharp" 100 20 UInt64.MaxValue |> Seq.map getTweet
-    printfn "%A" fsharpTweets
-
-    for tweet in fsharpTweets do
-        if session.Query<Tweet>().Any(fun t -> t.StatusId = tweet.StatusId) then
-            printfn "Duplicated tweet : %d %s" tweet.StatusId tweet.Text
-        else
-            printfn "Add tweet : %d %s" tweet.StatusId tweet.Text
-            session.Store(tweet)
-
+    printfn "%d new %s tweets" newTweets.Length tweetType
     session.SaveChanges()
 
 [<EntryPoint>]
@@ -196,7 +181,16 @@ let main argv =
     docStore.Conventions.MaxNumberOfRequestsPerSession <- 5000
     use session = docStore.OpenSession()
 
-    addNewFollowers session
-    addNewTweets session
-    addNewFSharpTweets session
+    saveNewFollowers session
+
+    let screenName = "kimsk"    
+    (getTweetsByScreenName screenName 5) 
+    |> List.map toTweet
+    |> saveNewTweets session screenName
+
+    let hashtag = "#fsharp"    
+    getStatuses "#fsharp" 100 20 UInt64.MaxValue 
+        |> List.map toTweet
+        |> saveNewTweets session hashtag
+
     0 // return an integer exit code
